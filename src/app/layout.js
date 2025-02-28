bfe233efc85a63dd2721afc1ac11231b3d00eaaf
Hairun Huang <hairunhuang@gmail.com>
2025-02-27 21:56:24 -0800

    clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -27,6 +27,19 @@ export const metadata = constructMetadata();
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className='!scroll-smooth'>
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qgr6eaa0hc");
+            `
+          }}
+        />
+      </head>
       <body className={cn('min-h-screen font-sans antialiased', mulish.variable, nunito.variable, bebas_neue.variable)}>
         <NextTopLoader color="#dc2625" showSpinner={false} />
         <Providers>
